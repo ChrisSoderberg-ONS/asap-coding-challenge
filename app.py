f8e5148d2746c0907159a8fc042627901240c28f
soderc <christoffer.soderberg@ons.gov.uk>
2025-03-26 16:58:48 +0000

    removed map generating code form app.py, additional app route to collect user input on the server, unsused for now

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from mapping.folium_map import *
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
+    return render_template("home.html")
 
-    gdf = create_gdf()
-    m = generate_map_from_gdf(gdf, w=800, h=600)
 
-    m.get_root().width = "800px"
-    m.get_root().height = "600px"
-    iframe = m.get_root()._repr_html_()
-
-    return render_template("home.html", iframe=iframe)
+@app.route("/slider_update", methods=["POST"])
+def slider():
+    received_data = request.data
+    return received_data
